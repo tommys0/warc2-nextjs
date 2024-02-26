134f69c81a3043aa3ad3accc7b17cff471d93977
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-02-26 17:21:02 +0100

    Page structure preparation

---
+++ my-first-app/src/app/games/[id]/edit/page.js
@@ -0,0 +1,23 @@
+import styles from './page.module.css';
+import { gamesData } from "@/app/games/page";
+import Link from "next/link";
+
+export default function Page({ params }) {
+    const selectedGame = gamesData.find(game => game.id === parseInt(params.id));
+
+    if (!selectedGame) {
+        return <p>Game not found</p>;
+    }
+
+    return (
+        <div className={styles.gameDetails}>
+            <h2 className={styles.title}>{selectedGame.title}</h2>
+            <p className={styles.genre}><strong>Genre:</strong> {selectedGame.genre}</p>
+            <p className={styles.releaseDate}><strong>Release Date:</strong> {selectedGame.release_date}</p>
+            <p className={styles.description}><strong>Description:</strong> {selectedGame.description}</p>
+            <Link href={`/games/`}>
+                <p className={styles.goBackButton}>Go Back</p>
+            </Link>
+        </div>
+    );
+}

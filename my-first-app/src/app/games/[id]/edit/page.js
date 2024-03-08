f2fff321eab23071d1b206f6234c0259b5d7eb3a
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-03-08 12:35:04 +0100

    Edit page re-do

--- my-first-app/src/app/games/[id]/edit/page.js
+++ my-first-app/src/app/games/[id]/edit/page.js
@@ -1,25 +1,56 @@
+"use client"
+
 import styles from './page.module.css';
 import { gamesData } from "@/app/games/page";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Page({ params }) {
     const selectedGame = gamesData.find(game => game.id === parseInt(params.id));
+    const [gameTitle, setGameTitle] = useState(selectedGame.title);
+    const [gameGenre, setGameGenre] = useState(selectedGame.genre);
+    const [gameReleaseDate, setGameReleaseDate] = useState(selectedGame.release_date);
+    const [gameDescription, setGameDescription] = useState(selectedGame.description);
 
     if (!selectedGame) {
         return (
             <p>Game not found.</p>
-        )
+        );
+    }
+
+    function saveData() {
+
     }
 
     return (
-        <div className={styles.gameDetails}>
-            <h2 className={styles.title}>{selectedGame.title}</h2>
-            <p className={styles.genre}><strong>Genre:</strong> {selectedGame.genre}</p>
-            <p className={styles.releaseDate}><strong>Release Date:</strong> {selectedGame.release_date}</p>
-            <p className={styles.description}><strong>Description:</strong> {selectedGame.description}</p>
-            <Link href={`/games/`}>
-                <p className={styles.goBackButton}>Go Back</p>
-            </Link>
+        <div className={styles.editForm}>
+            <label>
+                <input
+                    type="text"
+                    className={styles.formInputField}
+                    value={gameTitle}
+                    onChange={(e) => setGameTitle(e.target.value)}
+                />
+                <input
+                    type="text"
+                    className={styles.formInputField}
+                    value={gameGenre}
+                    onChange={(e) => setGameGenre(e.target.value)}
+                />
+                <input
+                    type="text"
+                    className={styles.formInputField}
+                    value={gameReleaseDate}
+                    onChange={(e) => setGameReleaseDate(e.target.value)}
+                />
+                <input
+                    type="text"
+                    className={styles.formInputField}
+                    value={gameDescription}
+                    onChange={(e) => setGameDescription(e.target.value)}
+                />
+            </label>
+            <button onClick={saveData}>Save</button>
         </div>
     );
 }
